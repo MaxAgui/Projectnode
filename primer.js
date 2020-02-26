9d08c821d07999aa6917dacaebae05062a968a5c
manuel <manuelmaximo19@gmail.com>
2020-02-26 02:14:24 -0500

    Primer commit

---
+++ primer.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const express = require('express')
+
+const server = express()
+
+server.use(express.static('public'))
+
+//server.get('/', function(req, res) {
+//   res.send('Hello world!')
+
+//})
+
+server.listen(3000)
